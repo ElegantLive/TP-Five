64cfe0fd4390bb88d15a02fe5ae99733ae6ee8a6
caixian <qucaixian@qq.com>
2018-04-29 19:25:39 +0800

    基类模型初步布置

--- application/api/model/BaseModel.php
+++ application/api/model/BaseModel.php
@@ -13,5 +13,31 @@ use think\Model;
 
 class BaseModel extends Model
 {
+    protected $auto = ['password'];
+    protected $hidden = ['delete_time','password'];
+    protected $readonly = ['name'];
+
+    protected function setPasswordAttr($value)
+    {
+        return md5($value);
+    }
+
+    protected function getStatusAttr($value)
+    {
+        $status = [
+            'NORMAL' => '正常',
+            'SUCCESS' => '成功',
+            'PROCESS' => '处理中',
+            'CANCEL' => '取消',
+            'WAIT_VERIFY' => '待审核'
+        ];
+
+        return $status[$value];
+    }
+
+    public static function pages(array $map,string $field = '*',string $order = 'create_time desc',int $page = 1,int $limit = 10 ,bool $simple = false)
+    {
+        return self::where($map)->field($field)->order($order)->paginate($limit,$simple,['page'=>$page]);
+    }
 
 }
