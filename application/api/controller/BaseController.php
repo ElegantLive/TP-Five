66d9afe29338eaabfd60a88e03806e5e623e7d36
caixian <qucaixian@qq.com>
2018-05-03 18:58:32 +0800

    重写TP前置方法

--- application/api/controller/BaseController.php
+++ application/api/controller/BaseController.php
@@ -24,4 +24,47 @@ class BaseController extends Controller
     {
         Token::authentication($Identity);
     }
+
+    /**
+     * 重写thinkphp的前置操作
+     * @param string $method
+     * @param array $options
+     */
+    protected function beforeAction($method, $options = [])
+    {
+        if (isset($options['only'])) {
+            if (is_string($options['only'])) {
+                $options['only'] = explode(',', $options['only']);
+            }
+
+            $new_arr = [];
+
+            foreach ($options['only'] as $action) {
+                $new_arr[] = uncamelize($action, '');
+            }
+
+            $options['only'] = $new_arr;
+
+            if (!in_array(strtolower($this->request->action()), $options['only'])) {
+                return;
+            }
+        } elseif (isset($options['except'])) {
+            if (is_string($options['except'])) {
+                $options['except'] = explode(',', $options['except']);
+            }
+
+            $new_arr = [];
+
+            foreach ($options['except'] as $action) {
+                $new_arr[] = uncamelize($action,'');
+            }
+
+            $options['except'] = $new_arr;
+            if (in_array(strtolower($this->request->action()), $options['except'])) {
+                return;
+            }
+        }
+        call_user_func([$this, $method]);
+    }
+
 }
